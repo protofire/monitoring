511f09623ececafc7eceb1551bef6955a7764d1e
Veaceslav Doina <20563034+veaceslavdoina@users.noreply.github.com>
2021-09-30 08:40:02 +0300

    Update docker_install.sh

--- logstash/binance/docker_install.sh
+++ logstash/binance/docker_install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+# Docker
 sudo amazon-linux-extras install -y docker
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo chkconfig docker on
 
-
+# Docker compose
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose version
