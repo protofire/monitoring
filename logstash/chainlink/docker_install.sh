7d673291ec9c05a84a59dc425f695c382fe4e3e0
Veaceslav Doina <20563034+veaceslavdoina@users.noreply.github.com>
2021-09-30 08:39:09 +0300

    Update docker_install.sh

--- logstash/chainlink/docker_install.sh
+++ logstash/chainlink/docker_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Docker compose
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose version
