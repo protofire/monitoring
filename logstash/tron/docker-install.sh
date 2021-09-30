e3ae78dd2254505f1c0766396f7af1086e043b2f
Veaceslav Doina <20563034+veaceslavdoina@users.noreply.github.com>
2021-09-30 08:38:40 +0300

    Update docker-install.sh

--- logstash/tron/docker-install.sh
+++ logstash/tron/docker-install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Docker
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
@@ -7,6 +8,9 @@ echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] 
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo usermod -a -G docker $USER
+
+# Docker compose
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose version
